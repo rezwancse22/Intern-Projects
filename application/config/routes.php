c5b6db5f3722de6960dc6b860a64dba6659f74d7
S.M REZWAN <rezwan.aiub.2022@gmail.com>
2026-07-30 12:28:58 +0600

    Updated routing configuration to set default controller and added new routes for home, about, services, and contact pages

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'pages/home';
+$route['home'] = 'pages/home';
+$route['about'] = 'pages/about';
+$route['services'] = 'pages/services';
+$route['contact'] = 'pages/contact';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
